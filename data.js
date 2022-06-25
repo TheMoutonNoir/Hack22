e3a305403400354a6e6f86e3f178043d0d8b2eed
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-06-25 17:59:46 -0400

    Added data

---
+++ data.js
@@ -0,0 +1,154 @@
+const data = [
+    {
+        name: 'More efficient vehicle',
+        value: 2.2,
+    },
+    {
+        name: 'Purchase alternative fuel vehicle',
+        value: 2.3,
+    },
+    {
+        name: 'Purchase electric vehicle',
+        value: 6.9,
+    },
+    {
+        name: 'Purchase hybrid vehilce',
+        value: 3.2,
+    },
+    {
+        name: 'Telecommute to work',
+        value: 1.0,
+    },
+    {
+        name: 'Ride my bike',
+        value: 0.5,
+    },
+    {
+        name: 'Take public transportation',
+        value: 0.4,
+    },
+    {
+        name: 'Practice eco driving',
+        value: 1.0,
+    },
+    {
+        name: 'Maintain my vehicles',
+        value: 0.8,
+    },
+    {
+        name: 'Carpool to work',
+        value: 0.9,
+    },
+    {
+        name: 'Stop travelling by plane',
+        value: 1.5,
+    },
+    {
+        name: 'Switch from incandescent bulbs to CFLs or LEDs',
+        value: 0.3,
+    },
+    {
+        name: 'Turn off lights',
+        value: 0.2,
+    },
+    {
+        name: 'Switch from T12 Lights to T8 Lights',
+        value: 0.1,
+    },
+    {
+        name: 'Purchase tankless water heater',
+        value: 0.0,
+    },
+    {
+        name: 'Reduce winter household thermostat by 8 degrees F',
+        value: 0.5,
+    },
+    {
+        name: 'Increase summer household thermostat by 4 degrees F',
+        value: 0.2,
+    },
+    {
+        name: 'Purchase high efficiency cooling',
+        value: 0.0,
+    },
+    {
+        name: 'Purchase high efficiency heating',
+        value: 0.0,
+    },
+    {
+        name: 'Replace refrigerator with Energy Star refrigerator',
+        value: 0.1,
+    },
+    {
+        name: 'Replace printers with Energy Star printers',
+        value: 0.0,
+    },
+    {
+        name: 'Replace copiers with Energy Star copiers',
+        value: 0.1,
+    },
+    {
+        name: 'Replace computers with Energy Star computers',
+        value: 0.0,
+    },
+    {
+        name: 'Use rechargeable batteries',
+        value: 0.2,
+    },
+    {
+        name: 'Manage computer energy use',
+        value: 0.4,
+    },
+    {
+        name: 'Purchase electricity from a green source',
+        value: 6.5,
+    },
+    {
+        name: 'Install photovoltaic (PV) panels',
+        value: 6.5,
+    },
+    {
+        name: 'Install solar water heater',
+        value: 0.0,
+    },
+    {
+        name: 'Install low flow showerheads',
+        value: 0.4,
+    },
+    {
+        name: 'Install low flow faucets',
+        value: 0.0,
+    },
+    {
+        name: 'Install low flow toilets',
+        value: 0.0,
+    },
+    {
+        name: 'Line dry clothing',
+        value: 0.4,
+    },
+    {
+        name: 'Install water efficient landscaping',
+        value: 0.0,
+    },
+    {
+        name: 'Plant trees',
+        value: 0.3,
+    },
+    {
+        name: 'Reduce my trash by 5%',
+        value: 0.4,
+    },
+    {
+        name: 'Print double sided',
+        value: 0.3,
+    },
+    {
+        name: 'Eat a low carbon diet',
+        value: 1.7,
+    },
+    {
+        name: 'Go organic',
+        value: 0.2,
+    },
+];
