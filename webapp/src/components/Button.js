43f378b2f5373a9f574eb3b70441aea0df42c284
EMS7 <rony-estefan.maheux-saban@dawsoncollege.qc.ca>
2022-06-25 16:48:02 -0400

    create react app

---
+++ webapp/src/components/Button.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function Example() {
+  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  const [count, setCount] = useState(0);
+
+  const dude = () => {
+    alert('HEY MAN!')
+  }
+  
+  function dudettes(){
+    alert('HEY WOMAN')
+  }
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => dudettes()}>
+        Click Here
+      </button>
+    </div>
+  );
+}
+
+export default Example
---
+++ webapp/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Example from 'components/Button'
+
+export default () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>Welcome to React-Static</h1>
+    <Example></Example>
+  </div>
+)
