33282f6f1ed380b7b41f73c7de047b122ac11141
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-06-26 00:57:53 -0400

    Started StartMenu

---
+++ webapp/src/components/StartMenu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const StartMenu = () => {
+    return (
+        <div className="startMenu">
+            <h1 className="title">Higher,</h1>
+            <h1 className="title">Higher!</h1>
+
+            <h2>Which action saves more tons of CO₂ per year?</h2>
+            <h3>An interactive game which you can only beat by learning.</h3>
+
+            <button>Play!</button>
+        </div>
+    );
+};
+
+export default StartMenu;
--- webapp/src/pages/index.js
+++ webapp/src/pages/index.js
@@ -1,16 +1,13 @@
-import React from 'react'
-import Example from 'components/Button'
-import Choice from 'components/Choice.js'
+import React from 'react';
+import Example from 'components/Button';
+import Choice from 'components/Choice.js';
+import StartMenu from '../components/StartMenu';
 
 export default () => (
-
-
-  
-
-  
-  <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static</h1>
+    <div style={{ textAlign: 'center' }}>
+        {/* <h1>Welcome to React-Static</h1>
     <Example onClick = "pick(1)"></Example>
-    <Choice></Choice>
-  </div>
-)
+    <Choice></Choice> */}
+        <StartMenu></StartMenu>
+    </div>
+);
