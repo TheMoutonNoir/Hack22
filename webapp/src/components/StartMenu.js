34eb1cc104ed19bf2ee2e1b05aafa15f2b5b4415
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2022-06-26 02:22:52 -0400

    Finished? the start screen

--- webapp/src/components/StartMenu.js
+++ webapp/src/components/StartMenu.js
@@ -3,13 +3,18 @@ import React from 'react';
 const StartMenu = () => {
     return (
         <div className="startMenu">
-            <h1 className="title">Higher,</h1>
-            <h1 className="title">Higher!</h1>
-
-            <h2>Which action saves more tons of CO₂ per year?</h2>
-            <h3>An interactive game which you can only beat by learning.</h3>
-
-            <button>Play!</button>
+            <h1 className="title">
+                HIGHER,
+                <br />
+                HIGHER!
+            </h1>
+            <div className="subtitleContainer">
+                <h2>Which action saves more tons of CO₂ per year?</h2>
+                <h3>
+                    An interactive game which you can only beat by learning.
+                </h3>
+            </div>
+            <button className="play">Play!</button>
         </div>
     );
 };
